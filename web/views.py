90af021fd2d0f655ce6951d612cb1218915b82e1
Nikita <carp0w.n@yandex.com>
2025-02-12 16:19:41 +0500

    `Added Category model and views, updated templates for category display and error listing.`

--- web/views.py
+++ web/views.py
@@ -5,15 +5,18 @@ from .models import *
 from django.contrib.auth.decorators import login_required
 
 def login_view(request):
+
     return render(request, 'login.html')  # Render the login form if GET request
 
 def index(request):
     error = Error.objects.all()
+
     return render(request, 'index.html', { "errors" : error })
 
 @login_required
 def errorPage(request, id):
     error = Error.objects.get(id=id)
+
     return render(request, 'error.html', { "errors" : error })
 
 @login_required
@@ -26,5 +29,5 @@ def search(request):
 @login_required
 def category_page(request, id):
     category = Error.objects.filter(category=id)
-    
+
     return render(request, 'category.html', { "category" : category })

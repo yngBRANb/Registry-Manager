da99bc8fe7997fd55f9c8a7209c9c7310eb3c69e
Satoro Goyda <admin@example.com>
2025-02-13 20:38:19 +0000

    Перевод файла уведомлений

--- ci-notify.sh
+++ ci-notify.sh
@@ -2,6 +2,6 @@
 
 TIME="10"
 URL="https://api.telegram.org/bot7708142308:AAF7Igl56gq9xlB3WyZfKrhLPrWy40BiW3k/sendMessage"
-TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+TEXT="Статус деплоя: $1%0A%0AПроект:+$CI_PROJECT_NAME%0AСсылка:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0AВетка:+$CI_COMMIT_REF_SLUG%0A%0AГойда zzz SVO"
 
 curl -s --max-time $TIME -d "chat_id=895942691&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
